862a415763768ba34026935d5600aaf5cfb67a3c
Gibran Fasha Ghazanfar <gibran.fsh@gmail.com>
2024-02-17 11:34:36 +0700

    feat: add status and social media on hero section

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -2,6 +2,9 @@
 import Image from "next/image";
 import { useGlitch, GlitchHandle } from "react-powerglitch";
 import { TypeAnimation } from "react-type-animation";
+import { AcademicCapIcon, BriefcaseIcon } from "@heroicons/react/24/solid";
+import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
+import Link from "next/link";
 
 const HeroSection = () => {
   const glitch: GlitchHandle = useGlitch({
@@ -62,18 +65,64 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-white text-base sm:text-md lg:text-lg mb-6">
-            Enthusiastic, highly motivated, adaptive, and eager to learn and
-            grow professionally, Gibran is familiar with various aspects of
-            software engineering, web development, database management, project
-            management, and other related IT fields because of his Information
-            and System Technology background. Gibran also has good
-            problem-solving and collaborative skills proven by his experience of
-            participating in various competitions and activities.
+            I'm an enthusiastic, highly motivated, adaptive, eager to learn, and
+            grow professionally person. I'm also familiar with various aspects
+            of software engineering, web development, database management,
+            artifical intelligence, machine learning, deep learning, cyber
+            security, computer networks, project management and other related IT
+            fields because of my Information and System Technology background. I
+            also have a good problem-solving and collaborative skills proven by
+            my experience of participating in various competitions and
+            activities.
           </p>
-          <div>
+          <h1 className="font-bold text-transparent bg-clip-text bg-red-600 text-2xl sm:text-3xl mb-4">
+            Current Status
+          </h1>
+          <div className="flex flex-row items-center justify-center sm:justify-start">
+            <BriefcaseIcon className="h-6 w-6 text-white mr-2" />
+            <span className="text-white text-sm sm:text-md">
+              Backend Engineer Intern @{" "}
+              <span className="font-bold">PT DOT Indonesia</span>
+            </span>
+          </div>
+          <div className="flex flex-row items-center justify-center sm:justify-start mt-2">
+            <AcademicCapIcon className="h-6 w-6 text-white mr-2" />
+            <span className="text-white text-sm sm:text-md">
+              Penultimate Information System and Techology @{" "}
+              <span className="font-bold">Institut Teknologi Bandung</span>
+            </span>
+          </div>
+          <div className="flex items-center justify-center sm:justify-start mt-6">
             <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-white hover:bg-slate-200 text-black">
               Download CV
             </button>
+            {/* github */}
+            <Link
+              href="https://github.com/gibranfsh"
+              className="text-white hover:text-slate-200"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub className="h-8 w-8" />
+            </Link>
+            {/* linkedin */}
+            <Link
+              href="https://www.linkedin.com/in/gibran-fasha-ghazanfar"
+              className="text-white hover:text-slate-200"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin className="h-8 w-8 ml-4" />
+            </Link>
+            {/* instagram */}
+            <Link
+              href="https://www.instagram.com/gibranfg"
+              className="text-white hover:text-slate-200"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram className="h-8 w-8 ml-4" />
+            </Link>
           </div>
         </div>
         <div className="col-span-5 place-self-center mt-16 lg:mt-0">
