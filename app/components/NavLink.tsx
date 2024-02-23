6cb85047bd633d0d9163e6288813b4f3ca172645
Gibran Fasha Ghazanfar <gibran.fsh@gmail.com>
2024-02-23 14:30:24 +0700

    fix: navbar and navlink

--- app/components/NavLink.tsx
+++ app/components/NavLink.tsx
@@ -1,23 +1,12 @@
 import Link from "next/link";
 
-const NavLink = ({
-  href,
-  title,
-  isActive,
-}: {
-  href: string;
-  title: string;
-  isActive: boolean;
-}) => {
+const NavLink = ({ href, title }: { href: string; title: string }) => {
   return (
-    <Link href={href}>
-      <h1
-        className={`block py-2 pl-3 pr-4 text-[#ADB7BE] lg:text-md rounded md:p-0 hover:text-white ${
-          isActive ? "underline" : ""
-        }`}
-      >
-        {title}
-      </h1>
+    <Link
+      href={href}
+      className="block py-2 pl-3 pr-4 text-[#ADB7BE] lg:text-md rounded md:p-0 hover:text-white"
+    >
+      {title}
     </Link>
   );
 };
